63f8e2734cde2a22219e4c3088e2bc7fa79aea64
braveras <acunamatata1997@gmail.com>
2023-01-16 10:52:55 +0100

    Starting to create fragments for the register form

---
+++ app/src/main/java/com/vitaleats/FragmentForm1.java
@@ -0,0 +1,16 @@
+package com.vitaleats;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentForm1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form_1, container, false);
+    }
+}
