d5a6139a20a97b32a71ae8b8c0baefe64b11df98
Jacobo Sánchez <jacosanro@gmail.com>
2023-01-19 12:25:25 +0100

    commit new layout

--- app/src/main/java/com/vitaleats/Inicio.java
+++ app/src/main/java/com/vitaleats/Inicio.java
@@ -2,13 +2,35 @@ package com.vitaleats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Inicio extends AppCompatActivity {
+
+    ImageButton buttonalimentos, buttonrecetas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        buttonalimentos=findViewById(R.id.buttonalimentos);
+
+        buttonalimentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                paginaSiguiente();
+            }
+        });
+    }
+
+    public void paginaSiguiente() {
+        Intent intent=new Intent(this, Alimentos.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //no volver para atras
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/vitaleats/Recetas.java
@@ -0,0 +1,14 @@
+package com.vitaleats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recetas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recetas);
+    }
+}
