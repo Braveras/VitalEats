1c7456181fe3675649964946481dfc12e54af4b8
Braveras <acunamatata1997@gmail.com>
2023-02-08 19:46:48 +0100

    Working profile pic updater & deleter. Added fields to display username and mail

--- app/src/main/java/com/vitaleats/main/FragmentMain4.java
+++ app/src/main/java/com/vitaleats/main/FragmentMain4.java
@@ -1,124 +1,117 @@
 package com.vitaleats.main;
 
-import static android.app.Activity.RESULT_OK;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 
+import static android.app.Activity.RESULT_OK;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
 import com.vitaleats.R;
 import com.vitaleats.login.MainActivity;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.UUID;
 
 public class FragmentMain4 extends Fragment {
 
-    private static final int PICK_IMAGE_REQUEST = 1000;
-    private static final int CAMERA_REQUEST = 1888;
-    private Button cerrar, editar, eliminar;
-    private ImageView imagen;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button signout_btn;
+    private ImageView profilePicture;
     private StorageReference mStorageRef;
-    private String storage_path = "images/";
-    private StorageReference storageReference;
-    private ContentResolver contentResolver;
+    private FirebaseAuth firebaseAuth;
+    private String currentUserId;
+    private FirebaseUser user;
+    private TextView userTextView, mailTextView;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main4, container, false);
-        cerrar = view.findViewById(R.id.cerrar);
-        editar = view.findViewById(R.id.editar);
-        eliminar = view.findViewById(R.id.eliminar);
-        imagen = view.findViewById(R.id.imagen);
+        signout_btn = view.findViewById(R.id.signout_btn);
+        profilePicture = view.findViewById(R.id.profilePicture);
+        userTextView = view.findViewById(R.id.usernameTextView);
+        mailTextView = view.findViewById(R.id.mailTextView);
         mStorageRef = FirebaseStorage.getInstance().getReference();
-        contentResolver = getContentResolver();
 
-        loadImage();
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        currentUserId = user.getUid();
 
-        cerrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Cerrar sesión en Firebase
-                FirebaseAuth.getInstance().signOut();
-                // Redirigir al usuario a la pantalla de inicio de sesión
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
-                Toast.makeText(getActivity(), getString(R.string.closedSession), Toast.LENGTH_LONG).show();
-                getActivity().finish();
-            }
+        userTextView.setText(user.getDisplayName());
+        mailTextView.setText(user.getEmail());
+
+        Glide.with(this).load(user.getPhotoUrl()).into(profilePicture);
+
+        signout_btn.setOnClickListener(v -> {
+            // Sign-out from firebase
+            FirebaseAuth.getInstance().signOut();
+            // Start login activity
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(getActivity(), getString(R.string.closedSession), Toast.LENGTH_LONG).show();
+            // Set default values upon log-out
+            profilePicture.setImageBitmap(null);
+            userTextView.setText("");
+            getActivity().finish();
         });
 
-        editar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final CharSequence[] options = {"Tomar foto", "Elegir de galería"};
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("Elige una opción");
-                builder.setItems(options, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int item) {
-                        if (options[item].equals("Tomar foto")) {
-                            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                            startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                        } else if (options[item].equals("Elegir de galería")) {
-                            Intent pickImageIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                            pickImageIntent.setType("image/*");
-                            startActivityForResult(pickImageIntent, PICK_IMAGE_REQUEST);
+        profilePicture.setOnClickListener(view1 -> {
+            PopupMenu popup = new PopupMenu(getContext(), profilePicture);
+            popup.getMenuInflater().inflate(R.menu.updateordelete_profile_pic, popup.getMenu());
+            popup.setOnMenuItemClickListener(item -> {
+                switch (item.getItemId()) {
+                    case R.id.update:
+                        // Code to update the picture
+                        selectImageFromGallery();
+                        return true;
+                    case R.id.delete:
+                        // Code to delete the picture
+                        if (user.getPhotoUrl() == null) {
+                            //Picture is already null, what u gonna delete, idiot?
+                            Toast.makeText(getActivity(), "No hay ninguna foto!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            profilePicture.setImageBitmap(null);
+                            updateUserProfileWithImageUri(null);
                         }
-                    }
-                });
-                builder.show();
-            }
+                        return true;
+                    default:
+                        return false;
+                }
+            });
+            popup.show();
         });
 
-        eliminar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Obtener referencia a la ubicación de la imagen
-                StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                // Borrar la imagen
-                storageReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(getActivity(), "Imagen eliminada con éxito", Toast.LENGTH_SHORT).show();
-                        imagen.setImageBitmap(null);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getActivity(), "Error al eliminar la imagen", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
         return view;
     }
 
@@ -126,109 +119,67 @@ public class FragmentMain4 extends Fragment {
         return this.getContext().getContentResolver();
     }
 
+    private void selectImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Seleccionar Foto"), PICK_IMAGE_REQUEST);
+    }
+
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Uri selectedImage = data.getData();
+        if (requestCode == PICK_IMAGE_REQUEST) {
+            if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+                Uri selectedImageUri = data.getData();
+                // Convertir la imagen a PNG
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                byte[] dataBytes = baos.toByteArray();
 
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case CAMERA_REQUEST:
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-                    uploadImageToFirebase(imageBitmap);
-                    break;
-                case PICK_IMAGE_REQUEST:
-                    try {
-                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
-                        uploadImageToFirebase(bitmap);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                // Subir la imagen a Firebase Storage
+                StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+                StorageReference imagesRef = storageRef.child("documents/users/" + currentUserId + "/profile_picture.png");
+                UploadTask uploadTask = imagesRef.putBytes(dataBytes);
+                Task<Uri> urlTask = uploadTask.continueWithTask(task -> {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
                     }
-                    break;
-            }
-        }
-    }
-
-    private void saveProfilePicture(Bitmap imageBitmap) {
-        try {
-            FileOutputStream fos = getContext().openFileOutput("profile_picture.png", getContext().MODE_PRIVATE);
-            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-            fos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadImage() {
-        // Obtiene la ruta de la imagen guardada en el dispositivo
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        File imagePath = new File(getContext().getFilesDir(), "profile_picture.png");
-        if (imagePath.exists()) {
-            // Carga la imagen en el ImageView
-            Bitmap bitmap = BitmapFactory.decodeFile(imagePath.getAbsolutePath());
-            imagen.setImageBitmap(bitmap);
-        } else {
-            if (user != null) {
-                Bitmap bitmap = BitmapFactory.decodeFile(imagePath.getAbsolutePath());
-                imagen.setImageBitmap(bitmap);
-            } else {
-                // Carga la imagen predeterminada
-                imagen.setImageBitmap(null);
-            }
-        }
-    }
-
-    /*public void loadImage(String userId) {
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-        StorageReference imagesRef = storageRef.child("images/" + userId + "/profile_picture.png");
-
-        imagesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                Glide.with(getContext())
-                        .load(uri)
-                        .into(imagen);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                e.printStackTrace();
-            }
-        });
-    }*/
-
-    private void uploadImageToFirebase(Bitmap imageBitmap) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-        byte[] data = baos.toByteArray();
-
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images/" + UUID.randomUUID().toString());
-        storageReference.putBytes(data)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                // Imagen subida con éxito
-                                Toast.makeText(getActivity(), "Imagen subida con éxito", Toast.LENGTH_SHORT).show();
-                                Picasso.with(getActivity())
-                                        .load(uri)
-                                        .into(imagen);
-                                saveProfilePicture(imageBitmap);
-                                imagen.setImageBitmap(imageBitmap);
-                            }
-                        });
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Error al subir la imagen
-                        Toast.makeText(getActivity(), "Error al subir la imagen", Toast.LENGTH_SHORT).show();
+                    return imagesRef.getDownloadUrl();
+                }).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        // Guardar la referencia a la imagen en el objeto firebaseUser
+                        updateUserProfileWithImageUri(downloadUri);
+                        Glide.with(this).load(downloadUri).into(profilePicture);
+                    } else {
+                        // Mostrar un mensaje de error si no se puede subir la imagen a Firebase
+                        Toast.makeText(getActivity(), "Error al subir imagen", Toast.LENGTH_SHORT).show();
                     }
                 });
+            } else {
+                // Mostrar un mensaje de error o hacer algo si el usuario no seleccionó ninguna imagen
+                Toast.makeText(getActivity(), "Ninguna imagen seleccionada", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void updateUserProfileWithImageUri(@Nullable Uri imageUri) {
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setPhotoUri(imageUri).build();
+
+        firebaseAuth.getCurrentUser().updateProfile(profileUpdates).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Log.d(TAG, "User profile updated.");
+                Toast.makeText(getActivity(), "Foto de perfil actualizada", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.e(TAG, "Failed to update user profile.", task.getException());
+                Toast.makeText(getActivity(), "Error al actualizar perfil", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
